c39b412b76c5fc15dc664561b2b407f0a0fa49b8
Gabriel Beaudry <gabriel.bdry@gmail.com>
2021-04-02 18:03:44 -0400

    Migrate to null safety + fixes

--- lib/sliding_up_panel.dart
+++ lib/sliding_up_panel.dart
@@ -1,10 +1,8 @@
-/*
-Name: Akshath Jain
-Date: 3/30/19
-Purpose: Defines the package: sliding_up_panel
-Copyright: © 2019, Akshath Jain. All rights reserved.
-Licensing: More information can be found here: https://github.com/akshathjain/sliding_up_panel/blob/master/LICENSE
-*/
+// Name: Akshath Jain
+// Date: 3/30/19
+// Purpose: Defines the package: sliding_up_panel
+// Copyright: © 2019, Akshath Jain. All rights reserved.
+// Licensing: More information can be found here: https://github.com/akshathjain/sliding_up_panel/blob/master/LICENSE
 
 library sliding_up_panel;
 
